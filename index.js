6d86b821e0f1c8ecbe74c90c74fa8d16934214ee
Sachini Wijesinghe <sachini@thesanmark.com>
2023-06-13 16:16:06 +0530

    add update url

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ app.get('/contacts', async (req, res) => {
 });
 
 app.get('/update', async (req, res) => {
+    // http://localhost:3000/update?email=wijesinghesachini09@gmail.com
     const email = req.query.email;
 
     const getContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email&properties=email,nic_number`;
@@ -125,50 +126,6 @@ app.post('/update', async (req, res) => {
 
 });
 
-// app.get('/update', async (req, res) => {
-//     // http://localhost:3000/update?email=rick@crowbars.net
-//     const email = req.query.email;
-
-//     const getContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email&properties=email,favorite_book`;
-//     const headers = {
-//         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-//         'Content-Type': 'application/json'
-//     };
-
-//     try {
-//         const response = await axios.get(getContact, { headers });
-//         const data = response.data;
-
-//         // res.json(data);
-//         res.render('update', {userEmail: data.properties.email, favoriteBook: data.properties.favorite_book});
-        
-//     } catch(err) {
-//         console.error(err);
-//     }
-// });
-
-// app.post('/update', async (req, res) => {
-//     const update = {
-//         properties: {
-//             "favorite_book": req.body.newVal
-//         }
-//     }
-
-//     const email = req.query.email;
-//     const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-//     const headers = {
-//         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-//         'Content-Type': 'application/json'
-//     };
-
-//     try { 
-//         await axios.patch(updateContact, update, { headers } );
-//         res.redirect('back');
-//     } catch(err) {
-//         console.error(err);
-//     }
-
-// });
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
